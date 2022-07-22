2d0e61614ce92227c59ef0eb48c70b4e312070d3
zagalexey <zagoralexey@gmail.com>
2022-07-22 23:15:16 +0200

    Added layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,86 @@
-import React, {useState} from 'react';
-import './App.css';
-import Navbar from "./components/Navbar";
-import Layout from "./components/Layout";
+import React, { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Layout from './components/Layout'
 
 type movieType = {
-    adult: boolean,
-    backdrop_path: string,
-    genre_ids: [],
-    original_language: string,
-    original_title: string,
-    overview: string,
-    popularity: number,
-    poster_path: string,
-    video: boolean,
-    vote_average: number,
-    vote_count: number,
-    id: number,
-    title: string,
-    release_date: string
+	adult: boolean
+	backdrop_path: string
+	genre_ids: []
+	original_language: string
+	original_title: string
+	overview: string
+	popularity: number
+	poster_path: string
+	video: boolean
+	vote_average: number
+	vote_count: number
+	id: number
+	title: string
+	release_date: string
 }
 
 function App() {
-    const API_KEY = 'e1a2c26e413c33d2bedda22168b9ca27'
-    const hrefAllMovies = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
+	const API_KEY = 'e1a2c26e413c33d2bedda22168b9ca27'
+	const hrefAllMovies = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
 
-    const [movies, setMovies] = useState<movieType[]>([])
-    // const [allSetUp, setAllSetUp] = useState(false)
+	const [movies, setMovies] = useState<movieType[]>([])
+	const [movieLayout, setMovieLayout] = useState<movieType>()
+	// const [allSetUp, setAllSetUp] = useState(false)
 
-    fetch(hrefAllMovies).then(response => {
-        return response.json()
-    }).then(data => {
-        setMovies(data.results)
-        // setAllSetUp(true)
-    })
+	fetch(hrefAllMovies)
+		.then(response => {
+			return response.json()
+		})
+		.then(data => {
+			setMovies(data.results)
+			setMovieLayout(data.results[0])
+			// setAllSetUp(true)
+		})
 
-    const onScroll = (): void => {
-        let elem = document.getElementsByTagName('nav')
-        let parentElem = document.querySelector('.main')
+	const onScroll = (): void => {
+		let elem = document.getElementsByTagName('nav')
+		let parentElem = document.querySelector('.main')
 
-        let positionObj = parentElem !== null ? parentElem.getBoundingClientRect() : null
-        if (positionObj !== null && positionObj.top !== 0) {
-            elem[0].classList.remove('navbarOnScrollOff')
-            elem[0].classList.add('navbarOnScrollOn')
-        } else {
-            elem[0].classList.remove('navbarOnScrollOn')
-            elem[0].classList.add('navbarOnScrollOff')
-        }
-    }
+		let positionObj =
+			parentElem !== null ? parentElem.getBoundingClientRect() : null
+		if (positionObj !== null && positionObj.top !== 0) {
+			elem[0].classList.remove('navbarOnScrollOff')
+			elem[0].classList.add('navbarOnScrollOn')
+		} else {
+			elem[0].classList.remove('navbarOnScrollOn')
+			elem[0].classList.add('navbarOnScrollOff')
+		}
+	}
 
-    window.onscroll = function () {
-        onScroll()
-    }
+	window.onscroll = function () {
+		onScroll()
+	}
 
-    return (
-        <>
-            {movies ?
-                <Layout title={movies[0].title}
-                        overview={movies[0].overview}
-                        imgPath={movies[0].backdrop_path}
-                        adult={movies[0].adult}>
-                    <Navbar/>
-                    <ul>
-                        {movies.map(item => (
-                            <>
-                                <li key={item.id}>{item.title}</li>
-                                <img style={{height: '100px', width: '200px'}}
-                                     key={Date.now()}
-                                     src={`https://image.tmdb.org/t/p/w300${item.backdrop_path}`}/>
-                            </>
-                        ))}
-                    </ul>
-                </Layout>
-                :
-                <h1>No data yet</h1>
-            }
-
-        </>
-
-    )
-        ;
+	return (
+		<>
+			<Layout
+				title={movieLayout ? movieLayout.title : null}
+				overview={movieLayout ? movieLayout?.overview : null}
+				imgPath={movieLayout ? movieLayout?.backdrop_path : null}
+				adult={movieLayout ? movieLayout?.adult : null}
+			>
+				<Navbar />
+				{/* <ul>
+					{movies.map(item => (
+						<>
+							<li key={item.id}>{item.title}</li>
+							<img
+								style={{ height: '100px', width: '200px' }}
+								key={Date.now()}
+								src={`https://image.tmdb.org/t/p/w300${item.backdrop_path}`}
+							/>
+						</>
+					))}
+				</ul> */}
+			</Layout>
+		</>
+	)
 }
 
-export default App;
+export default App
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,37 @@
-import React, {ReactNode} from 'react';
+import { JsxElement } from 'typescript'
 
-interface LayoutProps {
-    title: string,
-    overview: string,
-    imgPath: string,
-    adult: boolean,
-    children: any
+// interface LayoutProps {
+// 	title: string
+// 	overview: string
+// 	imgPath: string
+// 	adult: boolean
+// 	children: any
+// }
+
+type Props = {
+	title: string | null
+	overview: string | null
+	imgPath: string | null
+	adult: boolean | null
+	children?: JSX.Element | JSX.Element[] | string | string[]
 }
 
-const Layout = ({children, title, overview, imgPath, adult}: LayoutProps) => {
-    return (
-        <div className={'bg-blue-400 main'}>
-            {/*<img src={`https://image.tmdb.org/t/p/w300${imgPath}`}*/}
-            {/*     alt=""/>*/}
-            <p>{title}</p>
-            {children}
-        </div>
-    );
-};
+const Layout = ({ children, title, overview, imgPath, adult }: Props) => {
+	return (
+		<div className={'bg-blue-400 h-screen w-full relative main'}>
+			{/*<img src={`https://image.tmdb.org/t/p/w300${imgPath}`}*/}
+			{/*     alt=""/>*/}
 
-export default Layout;
+			{/* <p>{title}</p> */}
+			{children}
+			{title ? <p>{title}</p> : <></>}
+			<img
+				className={'w-full h-screen'}
+				src={`https://image.tmdb.org/t/p/w300${imgPath}`}
+				alt="test"
+			/>
+		</div>
+	)
+}
+
+export default Layout
